89d0a50f67bcde95d420ce883ddca3e5aa9d5b6a
Programmer Rakibul <rakibul00206@gmail.com>
2025-11-26 12:51:31 +0600

    single blog data get endpoint completed

--- src/app/api/blogs/[id]/route.js
+++ src/app/api/blogs/[id]/route.js
@@ -2,6 +2,50 @@ import { collections } from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
+export const GET = async (req, { params }) => {
+  const { id } = await params;
+
+  if (id.length !== 24) {
+    return NextResponse.json(
+      {
+        message: "Invalid ID",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const query = { _id: new ObjectId(id) };
+
+  try {
+    const { blogs } = await collections();
+    const result = await blogs.findOne(query);
+
+    if (result) {
+      return NextResponse.json({
+        success: true,
+        message: "Blog data retrieved successfully",
+        blogData: result,
+      });
+    }
+
+    return NextResponse.json(
+      {
+        message: "Blog data not found",
+      },
+      {
+        status: 404,
+      }
+    );
+  } catch (err) {
+    return NextResponse.json(
+      { success: false, message: err.message },
+      { status: 500 }
+    );
+  }
+};
+
 export const DELETE = async (req, { params }) => {
   const { id } = await params;
 
