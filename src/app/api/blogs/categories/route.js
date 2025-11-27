019e05f9d93416614b635b2b5ac1548e914574f9
Programmer Rakibul <rakibul00206@gmail.com>
2025-11-27 10:11:37 +0600

    get all categories data endpoint completed

---
+++ src/app/api/blogs/categories/route.js
@@ -0,0 +1,34 @@
+import { collections } from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  try {
+    const { blogs } = await collections();
+    const allBlogs = await blogs.find({}).toArray();
+    const categories = [
+      ...new Set(allBlogs.map((item) => item.category)),
+    ].reduce((acc, item, i) => {
+      if (item) {
+        acc.push({
+          id: i + 1,
+          name: item,
+        });
+      }
+
+      return acc;
+    }, []);
+
+    return NextResponse.json({
+      success: true,
+      message: "Category data retrieved successfully",
+      categories,
+    });
+  } catch (err) {
+    return NextResponse.json(
+      { success: false, message: err.message },
+      {
+        status: 500,
+      }
+    );
+  }
+};
